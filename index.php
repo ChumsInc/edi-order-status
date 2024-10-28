4e55b81bf9547c090e3f0191343f0c4acc52b94c
Steve Montgomery <steve@chums.com>
2024-10-28 12:05:13 -0600

    chore: Update to WebUI2

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-/**
- * @package Chums
- * @subpackage Routings
- * @author Steve Montgomery
- * @copyright Copyright &copy; 2012, steve
- */
+
+use chums\ui\WebUI2;
+use chums\user\Groups;
 
 require_once "autoload.inc.php";
-include_once "access.inc.php";
 
-$bodyPath = "/apps/edi-order-status";
-$title = "EDI Order Status";
-$ui = new WebUI($bodyPath, $title, '', true, 5);
-$ui->bodyClassName = 'container-fluid';
-$ui->addManifest("./public/js/manifest.json");
-$ui->Send();
+$ui = new WebUI2([
+    'bodyClassName' => 'container-fluid',
+    'title' => 'EDI Order Status',
+    'requiredRoles' => [Groups::PRODUCTION, Groups::CS],
+]);
+$ui->addManifestJSON('public/js/manifest.json')
+    ->render();
